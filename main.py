d75f36e80e17f52b3278e9155e5d7dc4d8990cee
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-08-01 15:16:40 +0600

    Day One

---
+++ main.py
@@ -0,0 +1,10 @@
+print('--- Home ---')
+print("""1- Add Todo
+2- show Todo
+3- Edit Todo
+4- Complete Todo
+5- Exit""")
+print("-----------------")
+
+user_action = input("Enter your option: ").capitalize()
+print(user_action)
